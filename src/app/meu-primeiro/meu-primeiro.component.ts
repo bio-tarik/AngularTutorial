460bff5cb23afabe131b0b33f4d2235f53403f92
tayoub <tarik.ayoub@i4pro.com.br>
2018-03-16 16:50:25 -0300

    Initial commit

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-componente',
+    template: `
+        <p>Meu primeiro componente com angular!</p>
+    `
+})
+export class MeuPrimeiroComponente{
+
+}
